9c1d3b800882c503c99d10e224a59fc97b6aea20
Caleb Lira <contatocaleb2010@gmail.com>
2026-07-17 01:39:11 -0300

    Adicionado caracteristicas do usuário

--- app/js/ui.js
+++ app/js/ui.js
@@ -10,7 +10,7 @@ export function renderUser(user) {
                 src="${user.avatar_url}"
                 class="w-28 h-28 rounded-full border-4 border-lime-400">
 
-            <h2 class="mt-5 text-3xl font-bold text-white">
+            <h2 class="font-ragick mt-5 text-3xl font-bold text-white">
                 ${user.name ?? user.login}
             </h2>
 
@@ -18,11 +18,35 @@ export function renderUser(user) {
                 @${user.login}
             </p>
 
+            <div class="mt-8 grid grid-cols-2 gap-4 md:grid-cols-4">
+
+    <div class="rounded-2xl bg-white/5 p-4 text-center border border-white/10">
+        <p class="text-2xl font-bold text-lime-400">${user.public_repos}</p>
+        <span class="text-sm text-zinc-400">Repositórios</span>
+    </div>
+
+    <div class="rounded-2xl bg-white/5 p-4 text-center border border-white/10">
+        <p class="text-2xl font-bold text-lime-400">${user.followers}</p>
+        <span class="text-sm text-zinc-400">Seguidores</span>
+    </div>
+
+    <div class="rounded-2xl bg-white/5 p-4 text-center border border-white/10">
+        <p class="text-2xl font-bold text-lime-400">${user.following}</p>
+        <span class="text-sm text-zinc-400">Seguindo</span>
+    </div>
+
+    <div class="rounded-2xl bg-white/5 p-4 text-center border border-white/10">
+        <p class="text-2xl font-bold text-lime-400">${user.public_gists}</p>
+        <span class="text-sm text-zinc-400">Gists</span>
+    </div>
+
+ </div>
+
         </div>
     `;
 }
 
-   function createRepositoryCard(repo, index) {
+function createRepositoryCard(repo, index) {
     return `
         <article
             class="reveal mt-10 rounded-3xl border border-white/10 bg-white/5 backdrop-blur-xl p-6 transition-all duration-300 hover:-translate-y-1 card-animation"
@@ -69,21 +93,24 @@ export function renderUser(user) {
     `;
 }
 
-
 export function renderRepositories(repositories) {
     results.innerHTML = "";
 
-    repositories.forEach((repo,index) => {
-        results.innerHTML += createRepositoryCard(repo,index);
+    repositories.forEach((repo, index) => {
+        results.innerHTML += createRepositoryCard(repo, index);
     });
 
     const card = document.querySelectorAll(".reveal");
 
-    cards.forEach((card,index) => {
+    cards.forEach((card, index) => {
         setTimeout(() => {
             card.classList.add("active");
         }, index * 80);
-    }); 
+    });
+}
 
+const counter = document.getElementById("repository-count");
 
+export function renderCounter(total) {
+    counter.textContent = `${total} repositórios encontrados`;
 }
